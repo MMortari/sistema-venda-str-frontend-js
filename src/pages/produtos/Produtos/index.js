9d24e63867e93b953af91112def188b63e82dbe4
Matheus <mmortari@lemontech.com.br>
2019-07-16 17:10:40 -0300

    Tela de Produtos - Cadastro e remoção

--- src/pages/produtos/Produtos/index.js
+++ src/pages/produtos/Produtos/index.js
@@ -1,6 +1,10 @@
 import React, { Component, Fragment } from 'react';
-import { Form, Modal, Button, Row, Col, Alert } from 'react-bootstrap';
+import { Form, Modal, Button, Row, Col } from 'react-bootstrap';
 import { Form as Unform, Select, Input } from '@rocketseat/unform';
+import { ToastContainer, toast } from 'react-toastify';
+import { find } from 'lodash';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 // Service
 import api from '../../../services/api';
@@ -10,6 +14,8 @@ import DinheiroMask from '../../../components/DinheiroMask'
 import Loading from '../../../components/Loading'
 //Style
 import './style.scss';
+
+const MySwal = withReactContent(Swal);
 
 class Produtos extends Component {
 
@@ -37,20 +43,78 @@ class Produtos extends Component {
     console.log("Initial Produtos State -> ", this.state)
   }
 
+  // Editar Produtos
   handleEditaProduto = (produto) => console.log("Editar Produto -> ", produto)
 
-  handleDeleteProduto = (id) => console.log("Apagar Produto -> ", id)
+  // Apagar Produto
+  handleDeleteProduto = id => {
+    MySwal.fire({
+      title: <p>Deseja apagar essa produto?</p>,
+      type: 'question',
+      showCancelButton: true,
+      cancelButtonText: "Cancel",
+      confirmButtonText: "Apagar"
+    }).then(async ({ value }) => {
+      if(value) {
+        let alert = toast("Pagando...", { containerId: 'A', autoClose: false });
 
+        const response = await api.delete(`/produtos/${id}`);
+
+        if(response.status === 200) {
+          toast.update(alert, {
+            render: 'Apagado com sucesso!',
+            type: toast.TYPE.SUCCESS,
+            containerId: 'A', 
+            autoClose: 5000
+          })
+
+          const produtos = this.state.produtos.filter(data => data.id !== id);
+          this.setState({ produtos })
+        } else {
+          toast.update(alert, {
+            render: 'Erro ao apagar!',
+            type: toast.TYPE.ERROR,
+            containerId: 'A', 
+            autoClose: 5000
+          })
+        }
+      }
+    })
+  }
+
+  // Adicionar Produtos
   handleOpenModalNewProduct = () => this.setState({ showModalNewProduct: true });
   handleCloseModalNewProduct = () => this.setState({ showModalNewProduct: false }); 
   handleSubmitModalNewProduct = async (data) => {
-    // console.log("Adicionar novo produtos -> ", data);
+    console.log("Adicionar novo produtos -> ", data);
+    let alert = toast("Adicionando...", { containerId: 'A', autoClose: false });
     const envia = await api.post(`/produtos`, {
       id: this.state.produtos[this.state.produtos.length] + 1,
       ...data,
-      created_at: "06/04/2019T09:16:32"
+      categoriaId: parseInt(data.categoriaId),
+      preco: parseFloat(data.preco),
+      created_at: new Date()
     })
-    console.log("Resposta adicionar novo produtos -> ", envia);
+
+    if(envia.status === 201 || envia.status === 200) {
+      toast.update(alert, {
+        render: 'Adicionado com sucesso!',
+        type: toast.TYPE.SUCCESS,
+        containerId: 'A', 
+        autoClose: 5000
+      })
+      this.setState({ produtos: [
+        ...this.state.produtos, 
+        { ...envia.data, showModalNewProduct: false, categoria: { nome: find(this.state.categorias, ["id", envia.data.categoriaId]).title } }
+      ] })
+    } else {
+      toast.update(alert, {
+        render: 'Erro ao adicionar!',
+        type: toast.TYPE.ERROR,
+        containerId: 'A', 
+        autoClose: 5000
+      })
+    }
   };
 
   render() {
@@ -61,7 +125,7 @@ class Produtos extends Component {
         </Head>
 
         { this.state.loading && (<Loading />) }
-        { this.state.error && (<Alert variant={"danger"}>error</Alert>) }
+        <ToastContainer enableMultiContainer containerId={'A'} position={toast.POSITION.TOP_RIGHT} />
 
         <table className="table table-borded">
           <thead>
@@ -81,7 +145,7 @@ class Produtos extends Component {
                 <td className="text-center">{produto.nome}</td>
                 <td className="text-center"><DinheiroMask>{produto.preco}</DinheiroMask></td>
                 <td className="text-center">{produto.categoria.nome}</td>
-                <td className="text-center"><button className="btn btn-info" onClick={() => this.handleEditaProduto(produto)}><i className="fa fa-info-circle"></i></button></td>
+                <td className="text-center"><button className="btn btn-info" onClick={() => this.handleEditaProduto(produto)}><i className="fa fa-edit"></i></button></td>
                 <td><button className="btn btn-danger" onClick={() => this.handleDeleteProduto(produto.id)}><i className="fa fa-trash"></i></button></td>
               </tr>
             )) }
@@ -106,19 +170,19 @@ class Produtos extends Component {
                   <Col md={12}>
                     <Form.Group controlId="formBasicPassword">
                       <Form.Label>Nome</Form.Label>
-                      <Input className="form-control" type="text" name="nome" placeholder="Insira o nome do produto" />
+                      <Input className="form-control" type="text" name="nome" placeholder="Insira o nome do produto" required />
                     </Form.Group>
                   </Col>
                   <Col md={6}>
                     <Form.Group controlId="formBasicPassword">
                       <Form.Label>Preço</Form.Label>
-                      <Input className="form-control" type="number" name="preco" placeholder="Insira o nome do produto" />
+                      <Input className="form-control" type="number" name="preco" placeholder="Insira o nome do produto" required />
                     </Form.Group>
                   </Col>
                   <Col md={6}>
                     <Form.Group controlId="formBasicPassword">
                       <Form.Label>Categoria</Form.Label>
-                      <Select className="form-control" name="categoriaId" options={this.state.categorias} placeholder="Selecione a categoria do produto" />
+                      <Select className="form-control" name="categoriaId" options={this.state.categorias} placeholder="Selecione a categoria do produto" required />
                     </Form.Group>
                   </Col>
                 </Row>
